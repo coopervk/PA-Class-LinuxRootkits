268654950ca1cdc2e06f767f1b806e14fc37638d
Cooper <coopervk@gmail.com>
2020-05-01 02:13:17 -0500

    Start formatted print LKM with altered template from basic helloworld

---
+++ 002-helloworld-fmt/helloworld-fmt.c
@@ -0,0 +1,18 @@
+#include <linux/module.h>
+#include <linux/init.h>
+
+MODULE_AUTHOR("Cooper Van Kampen");
+MODULE_LICENSE("GPL v2");
+MODULE_DESCRIPTION("Basic hello world module");
+
+static int __init lkm_init(void) {
+	pr_info("Hello to the cruel world of kernel programming\n");
+	return 0;
+}
+
+static void __exit lkm_exit(void) {
+	pr_info("Exiting hellwold lkm\n");
+}
+
+module_init(lkm_init);
+module_exit(lkm_exit);
