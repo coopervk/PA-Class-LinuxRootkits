448791cb12efd465ec5a3566bae4f2f61654b030
Cooper <coopervk@gmail.com>
2020-05-01 01:31:27 -0500

    Hello world LKM created and filled up

---
+++ 001-helloworld/helloworld.c
@@ -0,0 +1,19 @@
+#include <linux/module.h>
+#include <linux/init.h>
+
+MODULE_AUTHOR("Cooper Van Kampen");
+MODULE_LICENSE("GPL v2");
+MODULE_DESCRIPTION("Basic hello world module");
+
+static __init lkm_init(void) {
+	pr_info("Hello to the cruel world of kernel programming\n");
+	return 0;
+}
+
+static void __exit lkm_exit(void) {
+	pr_info("Exiting hellwold lkm\n");
+	return 0;
+}
+
+module_init(lkm_init);
+module_exit(lkm_exit);
