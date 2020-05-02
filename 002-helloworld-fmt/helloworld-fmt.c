e106de84ad2f838382e99a2e42e3578512745b95
Cooper <coopervk@gmail.com>
2020-05-02 00:32:36 -0500

    Add macro to make formatted printks with func and line in source

--- 002-helloworld-fmt/helloworld-fmt.c
+++ 002-helloworld-fmt/helloworld-fmt.c
@@ -5,6 +5,8 @@ MODULE_AUTHOR("Cooper Van Kampen");
 MODULE_LICENSE("GPL v2");
 MODULE_DESCRIPTION("Basic hello world module");
 
+#define pr_fmt(fmt)	KBUILD_MODNAME " -> %s:%d -> " fmt,__func__,__LINE__
+
 static int __init lkm_init(void) {
 	pr_info("Hello to the cruel world of kernel programming\n");
 	return 0;
