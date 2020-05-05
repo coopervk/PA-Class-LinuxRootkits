00a318f7eb0952cbe05994723ad736ab43b19ab7
Cooper <coopervk@gmail.com>
2020-05-04 22:45:50 -0500

    Start with altered template from 004

---
+++ 005-sysfs-hidden/sysfs-hidden.c
@@ -0,0 +1,32 @@
+#define pr_fmt(fmt)	KBUILD_MODNAME " -> %s:%d -> " fmt,__func__,__LINE__
+#include <linux/module.h>
+#include <linux/init.h>
+
+MODULE_AUTHOR("Cooper Van Kampen");
+MODULE_LICENSE("GPL v2");
+MODULE_DESCRIPTION("Basic hello world module");
+
+// Arguments
+static int counter;
+module_param(counter, int, 0);
+MODULE_PARM_DESC(counter, "Simple int based counter");
+
+static char *message = NULL;
+module_param(message, charp, 0);
+MODULE_PARM_DESC(message, "A message from user space");
+
+// "Main"
+static int __init lkm_init(void) {
+	pr_info("Arguments: counter = %d, message = %s\n", counter, message);
+
+	return 0;
+}
+
+// Exit routine
+static void __exit lkm_exit(void) {
+	pr_info("Exiting hellwold lkm\n");
+}
+
+// Define init and exit functions
+module_init(lkm_init);
+module_exit(lkm_exit);
