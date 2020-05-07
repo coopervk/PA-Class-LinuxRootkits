eba0eb55a4993c81a84f2043e304685c58d9248b
Cooper <coopervk@gmail.com>
2020-05-07 00:43:58 -0500

    Make library for simplifying calls to get process info from task_struct

---
+++ 006-process-info-basics/processlibrary.h
@@ -0,0 +1,31 @@
+#include <linux/sched.h>
+#include <linux/pid.h>
+#include <linux/cred.h>
+
+#define SUCCESS 1
+#define FAILURE 1
+#define PTR_NULL_CHECK(ptr)	if(ptr == NULL) return FAILURE;
+
+static inline struct task_struct* get_task_struct_by_pid(int pid) {
+	return pid_task(find_vpid(pid), PIDTYPE_PID);
+}
+
+static inline int print_task_pid_details(struct task_struct *task) {
+	PTR_NULL_CHECK(task);
+
+	pr_info("Task binary: %s\n", task->comm);
+	pr_info("TGID: %d\n", task->tgid);
+	pr_info("PID: %d\n", task->pid);
+
+	return SUCCESS;
+}
+
+static inline int print_task_parent_pid_details(struct task_struct *task) {
+	PTR_NULL_CHECK(task);
+
+	pr_info("Parent task binary: %s\n", task->real_parent->comm);
+	pr_info("Parent TGID: %d\n", task->real_parent->tgid);
+	pr_info("Parent PID: %d\n", task->real_parent->pid);
+
+	return SUCCESS;
+}
